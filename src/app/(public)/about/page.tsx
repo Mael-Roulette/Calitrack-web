7f730fa960193359b31792cf4b49a56daefe0907
cBasique <maelroulette12@gmail.com>
2025-09-25 11:48:42 +0200

    WEB : UI

---
+++ src/app/(public)/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About () {
+  return (
+    <section>
+      <h1 className="title-hero">À propos de Calitrack</h1>
+
+      {/* Section commencement */}
+      <div>
+        <h2 className="title-section font-calsans mb-3">Pourquoi <span className="text-secondary">Calitrack</span></h2>
+      </div>
+    </section>
+  )
+}
---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,14 @@
+import { Breadcrumb } from "@/components/ui/Breadcrumb";
+
+export default function RootLayout ( {
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}> ) {
+  return (
+    <section className="container mx-auto py-8 lg:pt-12 lg:pb-16 px-5 lg:px-0">
+      <Breadcrumb />
+      { children }
+    </section>
+  );
+}
