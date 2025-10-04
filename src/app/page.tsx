a63a61f73cd95a8d5c6330ebe17d22847cc01859
cBasique <maelroulette12@gmail.com>
2025-10-04 19:34:03 +0200

    WEB : fixed hero section on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,30 @@ import featuresInfos from "@/data/public/features.json";
 import milestones from "@/data/public/milestones.json";
 import Image from 'next/image';
 import Link from "next/link";
+import { FaChevronDown } from 'react-icons/fa6';
 
 export default function Home () {
   return (
     <main>
       {/* Section hero */ }
-      <section className="min-h-screen bg-gradient-to-t lg:bg-radial-[at_50%_90%] from-[#4E63D7] from-3% to-foreground to-80% lg:to-70% px-2">
+      <section className="relative min-h-[calc(100dvh-88px)] bg-gradient-to-t lg:bg-radial-[at_50%_90%] from-[#4E63D7] from-3% to-foreground to-80% lg:to-70% px-2">
         <h1 className="font-calsans text-background pt-16 lg:pt-24 mx-auto max-w-5xl text-center text-3xl lg:text-7xl">Tes objectifs, ta discipline Ton app, <span>Calitrack</span></h1>
         <p className="text-background mt-8 max-w-3xl mx-auto text-center text-lg lg:text-xl">Suis tes progrès, planifie tes séances et avance pas à pas vers tes objectifs en calisthenie.</p>
         <div className="mt-5 pb-20 flex flex-wrap justify-center items-center gap-4">
           <Link href="/contact" aria-label="Télécharger l'application" className="btn-primary">Tester la bêta</Link>
           <Link href="/features" aria-label="Découvrir les fonctionnalités" className="btn-secondary">En savoir plus</Link>
         </div>
+
+        <div className='absolute bottom-8 left-1/2 -translate-x-1/2'>
+          <a href="#features" className='text-background font-sora font-semibold flex flex-col items-center justify-center gap-1 transition-transform duration-300 hover:translate-y-2'>
+            <span>En savoir plus</span>
+            <FaChevronDown size={ 32 } className='text-background' />
+          </a>
+        </div>
       </section>
 
       {/* Section features */ }
-      <section>
+      <section id='features'>
         <h2 className="title-section font-calsans mt-8 mb-6 lg:mt-16 lg:mb-10 text-center">Tout ce dont tu as besoin pour <span>progresser</span></h2>
 
         <ul className="flex flex-row flex-wrap justify-center gap-x-5 gap-y-4 mb-8">
