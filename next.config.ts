b0914bf15bf5d65174ee73825510354f8de217f8
Mael <maelroulette12@gmail.com>
2025-11-15 11:06:00 +0100

    WEB: Updated next config to redirect

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,22 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   turbopack: {
-    root: path.join( __dirname, '..' ),
+    root: path.join(__dirname, '..'),
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'calitrack.appwrite.network',
+          },
+        ],
+        destination: 'https://calitrack.fr/:path*',
+        permanent: true,
+      },
+    ];
   },
 };
 
